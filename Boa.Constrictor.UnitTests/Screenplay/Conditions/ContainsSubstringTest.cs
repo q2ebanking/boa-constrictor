6f80f6f597d4f53499a43e7032d7acd2d787beee
Andrew Knight <AndyLPK247@gmail.com>
2020-10-23 18:13:24 -0400

    Fixing the broken unit test

--- Boa.Constrictor.UnitTests/Screenplay/Conditions/ContainsSubstringTest.cs
+++ Boa.Constrictor.UnitTests/Screenplay/Conditions/ContainsSubstringTest.cs
@@ -12,7 +12,7 @@ namespace Boa.Constrictor.UnitTests.Screenplay
         [Test]
         public void True()
         {
-            ContainsSubstring.Text("World").Evaluate("Hello World!").Should().BeFalse();
+            ContainsSubstring.Text("World").Evaluate("Hello World!").Should().BeTrue();
         }
 
         [Test]
