9e7060fa5011a3a593435b6a78ded82cec0f4c60
Andrew Knight <AndyLPK247@gmail.com>
2021-01-29 11:22:19 -0500

    Added IdAttribute Question

---
+++ Boa.Constrictor/WebDriver/Questions/IdAttribute.cs
@@ -0,0 +1,58 @@
+﻿using Boa.Constrictor.Screenplay;
+using OpenQA.Selenium;
+
+namespace Boa.Constrictor.WebDriver
+{
+    /// <summary>
+    /// Gets a web element's "id" attribute.
+    /// </summary>
+    public class IdAttribute : AbstractWebPropertyQuestion<string>
+    {
+        #region Constants
+
+        /// <summary>
+        /// The "id" attribute name.
+        /// </summary>
+        public const string Id = "id";
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Private constructor.
+        /// (Use static methods for public construction.)
+        /// </summary>
+        /// <param name="locator">The target Web element's locator.</param>
+        protected IdAttribute(IWebLocator locator) : base(locator, Id) { }
+
+        #endregion
+        
+        #region Builder Methods
+
+        /// <summary>
+        /// Constructs the question.
+        /// </summary>
+        /// <param name="locator">The target Web element's locator.</param>
+        /// <returns></returns>
+        public static IdAttribute Of(IWebLocator locator) => new IdAttribute(locator);
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Gets a web element's HTML attribute by name.
+        /// </summary>
+        /// <param name="actor">The actor.</param>
+        /// <param name="driver">The WebDriver.</param>
+        /// <returns></returns>
+        public override string RequestAs(IActor actor, IWebDriver driver)
+        {
+            actor.WaitsUntil(Existence.Of(Locator), IsEqualTo.True());
+            return driver.FindElement(Locator.Query).GetAttribute(PropertyName);
+        }
+        
+        #endregion
+    }
+}
