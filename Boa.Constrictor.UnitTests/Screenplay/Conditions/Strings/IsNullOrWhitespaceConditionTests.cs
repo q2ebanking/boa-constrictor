432bcaf8d62b95d0ad52b1f9de2d033d13995dc0
Andreas Willich <a.willich@tricentis.com>
2021-08-09 17:05:20 +0200

    add unit tests

---
+++ Boa.Constrictor.UnitTests/Screenplay/Conditions/Strings/IsNullOrWhitespaceConditionTests.cs
@@ -0,0 +1,26 @@
+﻿using Boa.Constrictor.Screenplay;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Boa.Constrictor.UnitTests.Screenplay
+{
+    [TestFixture]
+    public class IsNullOrWhitespaceConditionTests
+    {
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("\t")]
+
+        public void True(string actual)
+        {
+            IsNullOrWhitespaceCondition.Value().Evaluate(actual).Should().BeTrue();
+        }
+
+        [TestCase("actualvalue")]
+        public void False(string actual)
+        {
+            IsNullOrWhitespaceCondition.Value().Evaluate(actual).Should().BeFalse();
+        }
+    }
+}
