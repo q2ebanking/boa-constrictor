2b7cc6e3b4bd0a746558b6c468dd0d953b086cf3
Pandy Knight <andylpk.247@gmail.com>
2021-10-28 17:55:44 -0400

    Fixed RestApiCall regions

--- Boa.Constrictor/RestSharp/Interactions/RestApiCall.cs
+++ Boa.Constrictor/RestSharp/Interactions/RestApiCall.cs
@@ -40,69 +40,75 @@ namespace Boa.Constrictor.RestSharp
         /// <returns></returns>
         public override IRestResponse RequestAs(IActor actor) => CallRequest(actor);
 
-    //     /// <summary>
-    //     /// Checks if this restApiCall is equal to another restApiCall.
-    //     /// </summary>
-    //     /// <param name="obj">The other object.</param>
-    //     public override bool Equals(object obj)
-    //     {
-    //         bool same = obj is RestApiCall<TAbility> restApiCall &&
-    //             restApiCall.request.AlwaysMultipartFormData == obj.request.AlwaysMultipartFormData &&
-    //             restApiCall.request.JsonSerializer == obj.request.JsonSerializer &&
-    //             restApiCall.request.XmlSerializer == pbj.request.XmlSerializer &&
-    //             restApiCall.request.AdvancedResponseWriter == obj.request.AdvancedResponseWriter &&
-    //             restApiCall.request.ResponseWriter == obj.request.ResponseWriter &&
-    //             restApiCall.request.Method == obj.request.Method &&
-    //             restApiCall.request.Resource == obj.request.Resource &&
-    //             restApiCall.request.RequestFormat == obj.request.RequestFormat &&
-    //             restApiCall.request.RootElement == obj.request.RootElement &&
-    //             restApiCall.request.DateFormat == obj.request.DateFormat &&
-    //             restApiCall.request.XmlNamespace == obj.request.XmlNamespace &&
-    //             restApiCall.request.Credentials == obj.request.Credentials &&
-    //             restApiCall.request.Timeout == obj.request.Timeout &&
-    //             restApiCall.request.ReadWriteTimeout == obj.request.ReadWriteTimeout &&
-    //             restApiCall.request.Attempts == obj.request.Attempts &&
-    //             restApiCall.request.UseDefaultCredentials == obj.request.UseDefaultCredentials &&
-    //             restApiCall.request.OnBeforeRequest == obj.request.OnBeforeRequest &&
-    //             restApiCall.request.Body == obj.request.Body &&
-    //             restApiCall.request.AllowedDecompressionMethods.length == obj.request.AllowedDecompressionMethods.length &&
-    //             restApiCall.request.Files.length == obj.request.Files.length &&
-    //             restApiCall.request.Parameters.length == obj.request.Parameters.length
 
-    //         if (same) {
-    //             for(int i = 0; i < restApiCall.request.AllowedDecompressionMethods.length; i++) {
-    //                 same = same && restApiCall.request.AllowedDecompressionMethods[i].Equals(obj.request.AllowedDecompressionMethods[i])
-    //             }
 
-    //             for(int i = 0; i < restApiCall.request.Files.length; i++) {
-    //                 same = same && restApiCall.request.Files[i].Equals(obj.request.Files[i])
-    //             }
+        // The commented code should be used as the basis for:
+        // https://github.com/q2ebanking/boa-constrictor/issues/196
 
-    //             for(int i = 0; i < restApiCall.request.Parameters.length; i++) {
-    //                 same = same && restApiCall.request.Parameters[i].Equals(obj.request.Parameters[i])
-    //             }
-    //         }
+        //     /// <summary>
+        //     /// Checks if this restApiCall is equal to another restApiCall.
+        //     /// </summary>
+        //     /// <param name="obj">The other object.</param>
+        //     public override bool Equals(object obj)
+        //     {
+        //         bool same = obj is RestApiCall<TAbility> restApiCall &&
+        //             restApiCall.request.AlwaysMultipartFormData == obj.request.AlwaysMultipartFormData &&
+        //             restApiCall.request.JsonSerializer == obj.request.JsonSerializer &&
+        //             restApiCall.request.XmlSerializer == pbj.request.XmlSerializer &&
+        //             restApiCall.request.AdvancedResponseWriter == obj.request.AdvancedResponseWriter &&
+        //             restApiCall.request.ResponseWriter == obj.request.ResponseWriter &&
+        //             restApiCall.request.Method == obj.request.Method &&
+        //             restApiCall.request.Resource == obj.request.Resource &&
+        //             restApiCall.request.RequestFormat == obj.request.RequestFormat &&
+        //             restApiCall.request.RootElement == obj.request.RootElement &&
+        //             restApiCall.request.DateFormat == obj.request.DateFormat &&
+        //             restApiCall.request.XmlNamespace == obj.request.XmlNamespace &&
+        //             restApiCall.request.Credentials == obj.request.Credentials &&
+        //             restApiCall.request.Timeout == obj.request.Timeout &&
+        //             restApiCall.request.ReadWriteTimeout == obj.request.ReadWriteTimeout &&
+        //             restApiCall.request.Attempts == obj.request.Attempts &&
+        //             restApiCall.request.UseDefaultCredentials == obj.request.UseDefaultCredentials &&
+        //             restApiCall.request.OnBeforeRequest == obj.request.OnBeforeRequest &&
+        //             restApiCall.request.Body == obj.request.Body &&
+        //             restApiCall.request.AllowedDecompressionMethods.length == obj.request.AllowedDecompressionMethods.length &&
+        //             restApiCall.request.Files.length == obj.request.Files.length &&
+        //             restApiCall.request.Parameters.length == obj.request.Parameters.length
 
-    //         return same
-    //     }
-         #endregion
-     }
+        //         if (same) {
+        //             for(int i = 0; i < restApiCall.request.AllowedDecompressionMethods.length; i++) {
+        //                 same = same && restApiCall.request.AllowedDecompressionMethods[i].Equals(obj.request.AllowedDecompressionMethods[i])
+        //             }
 
-    //  /// <summary>
-    //     /// Gets a unique hash code for this RestApiCall.
-    //     /// </summary>
-    //     /// <returns></returns>
-    //     public override int GetHashCode()
-    //     {
-    //         HashCode.Combine(
-    //             GetType(), 
-    //             request.AlwaysMultipartFormData, 
-    //             request.Method,
-    //             request.Body,
-    //             request.Timeout,
-    //             request.Attempts,
-    //             request.RootElement)
-    //     }
+        //             for(int i = 0; i < restApiCall.request.Files.length; i++) {
+        //                 same = same && restApiCall.request.Files[i].Equals(obj.request.Files[i])
+        //             }
+
+        //             for(int i = 0; i < restApiCall.request.Parameters.length; i++) {
+        //                 same = same && restApiCall.request.Parameters[i].Equals(obj.request.Parameters[i])
+        //             }
+        //         }
+
+        //         return same
+        //     }
+
+        //     /// <summary>
+        //     /// Gets a unique hash code for this RestApiCall.
+        //     /// </summary>
+        //     /// <returns></returns>
+        //     public override int GetHashCode()
+        //     {
+        //         HashCode.Combine(
+        //             GetType(), 
+        //             request.AlwaysMultipartFormData, 
+        //             request.Method,
+        //             request.Body,
+        //             request.Timeout,
+        //             request.Attempts,
+        //             request.RootElement)
+        //     }
+
+        #endregion
+    }
 
     /// <summary>
     /// Calls the REST API given by the request spec and returns the response.
