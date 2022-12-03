2533e45dc67b611aab423ba5d7274e4761786f3d
Pandy Knight <andylpk.247@gmail.com>
2022-12-02 23:30:40 -0500

    Created Boa.Constrictor.Screenplay.UnitTests

---
+++ Boa.Constrictor.Screenplay.UnitTests/Screenplay/Conditions/Strings/IsNullOrWhitespaceTests.cs
@@ -0,0 +1,26 @@
+﻿using Boa.Constrictor.Screenplay;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Boa.Constrictor.Screenplay.UnitTests
+{
+    [TestFixture]
+    public class IsNullOrWhitespaceTests
+    {
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("\t")]
+
+        public void True(string actual)
+        {
+            IsNullOrWhitespace.Value().Evaluate(actual).Should().BeTrue();
+        }
+
+        [TestCase("actualvalue")]
+        public void False(string actual)
+        {
+            IsNullOrWhitespace.Value().Evaluate(actual).Should().BeFalse();
+        }
+    }
+}
