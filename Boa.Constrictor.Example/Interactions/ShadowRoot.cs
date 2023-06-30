d165a3bf8b63e30f7873984b3b84e2c1b1b8c6c5
Steve Hernandez <60014419+pl-shernandez@users.noreply.github.com>
2023-06-30 11:38:20 -0400

    Progress checkin

---
+++ Boa.Constrictor.Example/Interactions/ShadowRoot.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Boa.Constrictor.Screenplay;
+using OpenQA.Selenium;
+
+namespace Boa.Constrictor.Selenium
+{
+    /// <summary>
+    /// Gets the ShadowRoot for the locator.
+    /// </summary>
+    //public class ShadowRoot : AbstractWebLocatorQuestion<ISearchContext>
+    public class ShadowRoot : AbstractWebLocatorQuestion<string>
+    {
+        #region Constructors
+
+    /// <summary>
+    /// Private constructor.
+    /// (Use static methods for public construction.)
+    /// </summary>
+    /// <param name="locator">The target Web element's locator.</param>
+    private ShadowRoot(IWebLocator shadowRootLocator, WebLocator shadowContentLocator) : base(shadowContentLocator){
+        
+        ShadowRootLocator = shadowRootLocator;
+        ShadowContentLocator = shadowContentLocator;
+    } 
+
+    #endregion
+
+    #region Props
+
+    public IWebLocator ShadowRootLocator;
+    public WebLocator ShadowContentLocator;
+    
+    #endregion
+
+    #region Builder Methods
+
+    /// <summary>
+    /// Constructs the Question.
+    /// </summary>
+    /// <param name="locator">The target Web element's locator.</param>
+    /// <returns></returns>
+    public static ShadowRoot TextForShadowContentElement(IWebLocator shadowRootLocator, WebLocator shadowContentLocator) =>
+            new ShadowRoot(shadowRootLocator, shadowContentLocator);
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Gets the ShadowRoot for the locator.
+        /// </summary>
+        /// <param name="actor">The Screenplay Actor.</param>
+        /// <param name="driver">The WebDriver.</param>
+        /// <returns></returns>
+        //public override ISearchContext RequestAs(IActor actor, IWebDriver driver)
+        public override string RequestAs(IActor actor, IWebDriver driver)
+        {
+            ISearchContext shadowRoot = ShadowRootLocator.FindElement(driver).GetShadowRoot();
+            IWebElement shadowContentElement = shadowRoot.FindElement(ShadowContentLocator.Query);
+            string shadowElementText = shadowContentElement.Text;
+
+            //try
+            //{
+            //    ISearchContext shadowRoot = Locator.FindElement(driver).GetShadowRoot();
+
+
+            //    //return shadowRoot;
+            //}
+            //catch (Exception e)
+            //{
+            //    actor.Logger.Warning($"{this} not found");
+            //    actor.Logger.Warning(e.Message);
+            //    // throw;
+            //    return null;
+            //}
+            return shadowElementText;
+        }
+
+        /// <summary>
+        /// Returns a description of the Question.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() => $"ShadowRoot for '{Locator.Description}'";
+
+        #endregion
+    }
+}
---
+++ Boa.Constrictor.Example/Tests/ScreenplayWebUiShadowRootTest.cs
@@ -0,0 +1,119 @@
+﻿using Boa.Constrictor.Screenplay;
+using Boa.Constrictor.Selenium;
+using FluentAssertions;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using static Boa.Constrictor.Selenium.WebLocator;
+namespace Boa.Constrictor.Example
+{
+    public class ScreenplayWebUiShadowRootTest
+    {
+        private IActor Actor;
+
+        [SetUp]
+        public void InitializeScreenplay()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("headless");   // Remove this line to "see" the browser run
+            ChromeDriver driver = new ChromeDriver(options);
+
+            Actor = new Actor(name: "Andy", logger: new ConsoleLogger());
+            Actor.Can(BrowseTheWeb.With(driver));
+        }
+
+        [TearDown]
+        public void QuitBrowser()
+        {
+            Actor.AttemptsTo(QuitWebDriver.ForBrowser());
+        }
+
+        // Reference: See the image that visualizes the relationship between the DOM and the Shadow DOM https://www.lambdatest.com/blog/shadow-dom-in-selenium/
+
+
+        #region Locators
+
+        public const string Url = "http://watir.com/examples/shadow_dom.html";
+
+        //public static IWebLocator NonShadowHostDiv => L(
+        //    "ShadowHostDiv",
+        //    By.Id("non_host"));
+
+        public static IWebLocator ShadowHostDiv => L(
+          "ShadowHostDiv",
+          By.Id("shadow_host"));        
+        
+        public static WebLocator ShadowContentDiv => L(
+          "ShadowHostDiv",
+          By.ClassName("info"));
+
+        #endregion
+
+        [Test]
+        public void GetShadowRootContentText()
+        {
+            Actor.AttemptsTo(Navigate.ToUrl(ScreenplayWebUiShadowRootTest.Url));
+
+            string shadowContentText = Actor.AskingFor(Boa.Constrictor.Selenium.ShadowRoot.TextForShadowContentElement(ShadowHostDiv, ShadowContentDiv));
+            //ISearchContext shadowRoot = Actor.AskingFor(Selenium.ShadowRoot.ForShadowHost(ShadowHostDiv));
+            // ISearchContext shadowRoot = Actor.Using<BrowseTheWeb>().WebDriver.
+            //var a;
+
+            //shadowRoot.Should().NotBeNull();
+            shadowContentText.Should().Be("some text");
+        }
+
+
+        [Test]
+        public void GetShadowRootQuestionResult()
+        {
+            Actor.AttemptsTo(Navigate.ToUrl(ScreenplayWebUiShadowRootTest.Url));
+
+            string shadowContentText = Actor.AskingFor(Boa.Constrictor.Selenium.ShadowRoot.ForShadowHostElement(ShadowHostDiv, ShadowContentDiv));
+            //ISearchContext shadowRoot = Actor.AskingFor(Selenium.ShadowRoot.ForShadowHost(ShadowHostDiv));
+            // ISearchContext shadowRoot = Actor.Using<BrowseTheWeb>().WebDriver.
+            //var a;
+
+            //shadowRoot.Should().NotBeNull();
+            true.Should().BeTrue();
+        }
+
+        //[Test]
+        //public void NonShadowNull()
+        //{
+        //    Actor.AttemptsTo(Navigate.ToUrl(Url));
+
+        //    ISearchContext shadowRoot = Actor.AskingFor(Selenium.ShadowRoot.ForShadowHost(NonShadowHostDiv));
+
+        //    shadowRoot.Should().BeNull();
+        //}
+
+        //[Test]
+        //public void ShadowSpanText()
+        //{
+        //    Actor.AttemptsTo(Navigate.ToUrl(Url));
+
+        //    ISearchContext shadowRoot = Actor.AskingFor(Selenium.ShadowRoot.ForShadowHost(ShadowHostDiv));
+
+        //    IWebElement infoSpan = shadowRoot.FindElement(By.ClassName("info"));
+        //    infoSpan.Text.Should().Be("some text");
+        //}
+
+        //[Test]
+        //public void NestedShadowDivText()
+        //{
+        //    Actor.AttemptsTo(Navigate.ToUrl(Url));
+
+        //    ISearchContext shadowRoot = Actor.AskingFor(Selenium.ShadowRoot.ForShadowHost(ShadowHostDiv));
+        //    IWebElement nestedShadowHostDiv = shadowRoot.FindElement(By.CssSelector("#nested_shadow_host"));
+
+        //    ISearchContext nestedShadowHostDivShadowRoot = nestedShadowHostDiv.GetShadowRoot();
+        //    IWebElement nestedShadowContentDiv = nestedShadowHostDivShadowRoot.FindElement(By.Id("nested_shadow_content"));
+        //    string nestedShadowHostContentDivText = nestedShadowContentDiv.FindElement(By.TagName("div")).Text;
+
+        //    nestedShadowHostContentDivText.Should().Be("nested text");
+        //}
+
+
+    }
+}
