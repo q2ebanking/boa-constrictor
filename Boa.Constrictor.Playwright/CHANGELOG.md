be0afa60eff8b0a327a003ac312294bdbbd09ded
Keith T <ktremorin@rapidrtc.com>
2024-05-07 17:26:02 -0500

    Add SetChecked task

--- Boa.Constrictor.Playwright/CHANGELOG.md
+++ Boa.Constrictor.Playwright/CHANGELOG.md
@@ -13,7 +13,7 @@ Its format is based on Keep a Changelog, and this project adheres to Semantic Ve
 - Added `PlaywrightLocator` to store description + selector logic of a playwright locator
 - Added `AbstractPageTask` that makes it easier to write Tasks that use the BrowseTheWebSynchronously Ability
 - Added `AbstractLocatorTask` to make it easier to write tasks that perform operations on a single locator
-- Added `Click`, `Fill`, and `Go` Tasks
+- Added `Click`, `Fill`, `SetChecked` and `Go` Tasks
 - Added `OpenNewPage` task to initialize page and navigate to the specified url
 - Added `Text` and `Attribute` questions
 - Added an `Expects` extension method to `IActor` to make it easier to use playwrights built in `ILocatorAssertions`
---
+++ Boa.Constrictor.Playwright/Tasks/SetChecked.cs
@@ -0,0 +1,81 @@
+namespace Boa.Constrictor.Playwright
+{
+    using System.Threading.Tasks;
+    using Boa.Constrictor.Screenplay;
+    using Microsoft.Playwright;
+
+    /// <summary>
+    /// Sets the checked state of an element.
+    /// </summary>
+    public class SetChecked : AbstractLocatorTask
+    {
+        private readonly bool CheckedState;
+        private readonly LocatorSetCheckedOptions Options;
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="locator">The target element.</param>
+        /// <param name="checkedState">The checked state.</param>
+        /// <param name="options">Call options.</param>
+        private SetChecked(IPlaywrightLocator locator, bool checkedState, LocatorSetCheckedOptions options) : base(locator)
+        {
+            CheckedState = checkedState;
+            Options = options;
+        }
+
+        #endregion
+
+        #region Builder Methods
+
+        /// <summary>
+        /// Toggles the check state to on.
+        /// </summary>
+        /// <param name="locator">The target element.</param>
+        /// <param name="options">Call options.</param>
+        /// <returns></returns>
+        public static SetChecked On(IPlaywrightLocator locator, LocatorSetCheckedOptions options = null)
+        {
+            return new SetChecked(locator, true, options);
+        }
+
+        /// <summary>
+        /// Toggles the check state to off.
+        /// </summary>
+        /// <param name="locator">The target element.</param>
+        /// <param name="options">Call options.</param>
+        /// <returns></returns>
+        public static SetChecked Off(IPlaywrightLocator locator, LocatorSetCheckedOptions options = null)
+        {
+            return new SetChecked(locator, false, options);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Sets the checked state of an element.
+        /// </summary>
+        /// <param name="actor">The Screenplay Actor.</param>
+        /// <param name="locator">The target locator.</param>
+        public override async Task PerformAsAsync(IActor actor, ILocator locator)
+        {
+            await locator.SetCheckedAsync(CheckedState, Options);
+        }
+
+        /// <summary>
+        /// Returns a description of the Task.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"Set checked state to {CheckedState} on {Locator.Description}";
+        }
+
+        #endregion
+
+    }
+}
