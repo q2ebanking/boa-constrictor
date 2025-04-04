ffdba8ca0eb31ba95b91f85d844fdcad9836f9cc
Keith T <ktremorin@rapidrtc.com>
2025-04-03 22:09:26 -0500

    Removed test that didn't comply with CodeQL

--- Boa.Constrictor.Example/Playwright/Tests/ScreenplayPlaywrightTest.cs
+++ Boa.Constrictor.Example/Playwright/Tests/ScreenplayPlaywrightTest.cs
@@ -3,6 +3,7 @@ using Microsoft.Playwright;
 
 namespace Boa.Constrictor.Example;
 
+using System.Text;
 using System.Threading.Tasks;
 using Boa.Constrictor.Playwright;
 using Boa.Constrictor.Screenplay;
@@ -29,38 +30,26 @@ public class ScreenplayPlaywrightTest
     public async Task CheckboxTest()
     {
         await Actor.AttemptsToAsync(OpenNewPage.ToUrl(CheckboxPage.Url));
-        
+
         await Actor.AttemptsToAsync(SetChecked.On(CheckboxPage.Checkbox1));
         await Actor.AttemptsToAsync(SetChecked.Off(CheckboxPage.Checkbox2));
 
         await Actor.Expects(CheckboxPage.Checkbox1).ToBeCheckedAsync();
         await Actor.Expects(CheckboxPage.Checkbox2).Not.ToBeCheckedAsync();
     }
-    
+
     [Test]
     public async Task DropdownTest()
     {
         await Actor.AttemptsToAsync(OpenNewPage.ToUrl(DropdownPage.Url));
-        
+
         await Actor.AttemptsToAsync(SelectOption.ByLabel(DropdownPage.Dropdown, "Option 1"));
         await Actor.Expects(DropdownPage.Dropdown).ToHaveValueAsync("1");
-        
+
         await Actor.AttemptsToAsync(SelectOption.ByValue(DropdownPage.Dropdown, "2"));
         await Actor.Expects(DropdownPage.Dropdown).ToHaveValueAsync("2");
-        
+
         await Actor.AttemptsToAsync(SelectOption.ByIndex(DropdownPage.Dropdown, 1));
         await Actor.Expects(DropdownPage.Dropdown).ToHaveValueAsync("1");
-    }
-
-    [Test]
-    public async Task LoginTest()
-    {
-        await Actor.AttemptsToAsync(OpenNewPage.ToUrl(LoginPage.Url));
-
-        await Actor.AttemptsToAsync(Fill.ValueIn(LoginPage.UsernameInput, "tomsmith"));
-        await Actor.AttemptsToAsync(Fill.ValueIn(LoginPage.PasswordInput, "SuperSecretPassword!"));
-        await Actor.AttemptsToAsync(Click.On(LoginPage.LoginButton));
-
-        await Actor.ExpectsPage().ToHaveURLAsync(new Regex(".*/secure"));
     }
 }
